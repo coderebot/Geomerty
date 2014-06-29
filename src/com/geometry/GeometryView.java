2d09f89aaece0b00483bef81f75078dee30fbfec
unknown <vecodo>
2014-06-29 09:37:58 +0800

    create

---
+++ src/com/geometry/GeometryView.java
@@ -0,0 +1,11 @@
+package com.geometry;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GeometryView extends View {
+	public GeometryView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
